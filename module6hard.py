fa0deb4da529896e4d7d009a80fd051d35016cc6
TimofeiKazakov <violet09502@gmail.com>
2024-11-22 16:41:53 +0300

    Add files via upload

--- module6hard.py
+++ module6hard.py
@@ -65,29 +65,27 @@ class Cube(Figure):
         super().__init__(color, sides, filled=filled)
 
     def get_volume(self):
-        side_length = self.get_sides()[0]  
+        side_length = self.get_sides()[0]
         return side_length ** 3
 
-if __name__ == "__main__":
 
-    circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
-    cube1 = Cube((222, 35, 130), 6)
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
 
-    # Проверка на изменение цветов:
-    circle1.set_color(55, 66, 77) # Изменится
-    print(circle1.get_color())
-    cube1.set_color(300, 70, 15) # Не изменится
-    print(cube1.get_color())
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
 
-    # Проверка на изменение сторон:
-    cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
-    print(cube1.get_sides())
-    circle1.set_sides(15) # Изменится
-    print(circle1.get_sides())
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15) # Изменится
+print(circle1.get_sides())
 
-    # Проверка периметра (круга), это и есть длина:
-    print(len(circle1))
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
 
-    # Проверка объёма (куба):
-    print(cube1.get_volume())
-
+# Проверка объёма (куба):
+print(cube1.get_volume())
